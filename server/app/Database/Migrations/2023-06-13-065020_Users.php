96cf2e4fcfa283cb9e3fb56596b4b5044e401f45
roxyzc <roxyzc12@gmail.com>
2023-06-13 14:45:21 +0700

    menambahkan login dan register

---
+++ server/app/Database/Migrations/2023-06-13-065020_Users.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Users extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'userId' => [
+                'type' => 'INT',
+                'unsigned'       => true,
+                'auto_increment' => true,
+                'constraint' => 11
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+                'unique' => true
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255'
+            ],
+            'role' => [
+                'type' => "ENUM",
+                'constraint' => "'user', 'admin'",
+                'default' => "user"
+            ],
+            'created_at' => [
+                'type' => 'BIGINT',
+                'constraint' => 20,
+                'null' => true
+            ],
+            'updated_at' => [
+                'type' => 'BIGINT',
+                'constraint' => 20,
+                'null' => true
+            ]
+        ]);
+
+        $this->forge->addKey('userId',  TRUE);
+        $this->forge->createTable('users', TRUE);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
