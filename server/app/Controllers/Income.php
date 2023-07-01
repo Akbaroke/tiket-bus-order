e3a66f41015112ca1c6d2e59541ee3b08cd7e802
roxyzc <roxyzc12@gmail.com>
2023-07-01 17:43:08 +0700

    update

--- server/app/Controllers/Income.php
+++ server/app/Controllers/Income.php
@@ -25,23 +25,32 @@ class Income extends ResourceController
     {
         $date1 =  Time::createFromTimestamp($this->request->getVar("date1"))->setTime(0, 0, 0)->getTimestamp();
         $date2 =  Time::createFromTimestamp($this->request->getVar("date2"))->setTime(0, 0, 0)->getTimestamp();
-        $busFleetId = $this->request->getVar("busFleetId");
         // $dateTime = $date->toLocalizedString('HH:mm:ss');
         try {
             $rules = [
                 "date1" => "required|integer|greater_than_equal_to[0]",
                 "date2" => "required|integer|greater_than_equal_to[0]",
-                "busFleetId" => "required"
             ];
             if (!$this->validate($rules)) return $this->fail($this->validator->getErrors());
-            $data = $this->IncomeModel->join("busFleet as b", "b.busFleetId = income.busFleetId")->where(["income.created_at >=" => $date1, "income.created_at <=" => $date2, "income.busFleetId" => $busFleetId])
+            $data = $this->IncomeModel->join("busFleet as b", "b.busFleetId = income.busFleetId")->where(["income.created_at >=" => $date1, "income.created_at <=" => $date2])
                 ->findAll();
 
-            if ($data == null) throw new \Exception("armada tidak ditemukan");
-            $income = ["aramada" => $data[0]["name"], "income" => 0, "totalPassengers" => 0];
+            if ($data == null) throw new \Exception("income tidak ditemukan");
+            $income = [];
             foreach ($data as $d) {
-                $income["income"] += $d["income"];
-                $income["totalPassengers"] += $d["totalPassengers"];
+                if (count($income) == 0) {
+                    array_push($income, ["busFleetId" => $d["busFleetId"], "aramada" => $d["name"], "income" => 0, "totalPassengers" => 0]);
+                    continue;
+                }
+
+                $index = array_search($d["busFleetId"], array_column($income, "busFleetId"));
+                if ($index === false) {
+                    array_push($income, ["busFleetId" => $d["busFleetId"], "aramada" => $d["name"], "income" => 0, "totalPassengers" => 0]);
+                    continue;
+                }
+
+                $income[$index]["income"] += $d["income"];
+                $income[$index]["totalPassengers"] += $d["totalPassengers"];
             }
 
             $response = [
